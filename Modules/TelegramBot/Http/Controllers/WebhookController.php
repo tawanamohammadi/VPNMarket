931018b52dce039ef5ee1920ccbb7199d4d61d30
tawanamohammadi <tawanamohammadi@gmail.com>
2026-02-24 23:06:01 +0330

    fix: escape parentheses in QR service details message (line 1758)

--- Modules/TelegramBot/Http/Controllers/WebhookController.php
+++ Modules/TelegramBot/Http/Controllers/WebhookController.php
@@ -1755,7 +1755,7 @@ class WebhookController extends Controller
             $remainingText = "*منقضی شده*";
             $statusIcon = '⚫️';
         } elseif ($daysRemaining <= 7) {
-            $remainingText = "*" . $this->escape($daysRemaining . ' روز') . "* باقی‌مانده (تمدید کنید)";
+            $remainingText = "*" . $this->escape($daysRemaining . ' روز') . "* باقی‌مانده \\(تمدید کنید\\)";
             $statusIcon = '🟡';
         } else {
             $remainingText = "*" . $this->escape($daysRemaining . ' روز') . "* باقی‌مانده";
